5e629e659b724d8ecdaef14382c2d84d9233fe8b
Akiya <autumnsky64@gmail.com>
2022-08-21 19:24:20 +0900

    タイトルと1要素のリスト表示・編集を実装

--- code.tsx
+++ code.tsx
@@ -1,8 +1,56 @@
 const { widget } = figma
-const { Frame } = widget
+const { AutoLayout,
+Input,
+useSyncedState} = widget
 
-function Widget() {
-  return <Frame width={100} height={100} fill={'#C4C4C4'}></Frame>
+function ThreeItemList() {
+
+  const [title, setTitle] = useSyncedState("title","")
+  const [items, setItem] = useSyncedState("items",[""])
+
+  return (
+    <AutoLayout
+      width={320}
+      direction="vertical"
+      verticalAlignItems="center"
+      horizontalAlignItems="center"
+      stroke="#ccc"
+    >
+      <AutoLayout
+        width="fill-parent"
+        padding={{
+          vertical: 16
+        }}
+        fill="#ada"
+      >
+        <Input
+          value={title}
+          width="fill-parent"
+          fontSize={24}
+          fill="#161"
+          horizontalAlignText="center"
+          onTextEditEnd={(event)=> setTitle(event.characters)}
+          />
+      </AutoLayout>
+      <AutoLayout
+        width="fill-parent"
+        padding={{
+          vertical: 16,
+          bottom:0
+        }}
+        fill="#fff"
+      >
+        <Input
+          value={items[0]}
+          width="fill-parent"
+          height={48}
+          fontSize={24}
+          horizontalAlignText="center"
+          onTextEditEnd={(event)=> setItem([event.characters])}
+          />
+      </AutoLayout>
+    </AutoLayout>
+
+  )
 }
-
-widget.register(Widget)
+widget.register(ThreeItemList)
