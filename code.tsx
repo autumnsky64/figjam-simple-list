43d2c0ef799b701885394d61de4a850bbbb842a0
Akiya <autumnsky64@gmail.com>
2022-08-22 06:57:02 +0900

    3つのリスト要素を扱えるよう実装

--- code.tsx
+++ code.tsx
@@ -1,12 +1,15 @@
 const { widget } = figma
-const { AutoLayout,
-Input,
-useSyncedState} = widget
+const {
+  AutoLayout,
+  Input,
+  Frame,
+  useSyncedState
+} = widget
 
 function ThreeItemList() {
-
-  const [title, setTitle] = useSyncedState("title","")
-  const [items, setItem] = useSyncedState("items",[""])
+  const placeholders = ["path", "template php", "memo"]
+  const [title, setTitle] = useSyncedState("title","title")
+  const [items, setItem] = useSyncedState("items",["", "", ""])
 
   return (
     <AutoLayout
@@ -22,33 +25,55 @@ function ThreeItemList() {
           vertical: 16
         }}
         fill="#ada"
+        stroke="#ada"
       >
         <Input
+          placeholder="title"
           value={title}
           width="fill-parent"
           fontSize={24}
-          fill="#161"
+          fill="#040"
           horizontalAlignText="center"
           onTextEditEnd={(event)=> setTitle(event.characters)}
           />
       </AutoLayout>
-      <AutoLayout
-        width="fill-parent"
-        padding={{
-          vertical: 16,
-          bottom:0
-        }}
-        fill="#fff"
-      >
-        <Input
-          value={items[0]}
-          width="fill-parent"
-          height={48}
-          fontSize={24}
-          horizontalAlignText="center"
-          onTextEditEnd={(event)=> setItem([event.characters])}
-          />
-      </AutoLayout>
+      {items.map((item,i) => {
+        return(
+        <>
+          <Frame
+            height={1}
+            width="fill-parent"
+            name="Border"
+            fill="#ccc"
+            hidden={i === 0 ? true : false}
+          >
+          </Frame>
+          <AutoLayout
+            width="fill-parent"
+            padding={{
+              vertical: 16,
+              bottom:0
+            }}
+            fill="#fff"
+            >
+            <Input
+              value={item}
+              placeholder={placeholders[i]}
+              width="fill-parent"
+              height={48}
+              fontSize={24}
+              horizontalAlignText="center"
+              onTextEditEnd={(event)=> {
+                const newItems = [...items]
+                newItems[i] = event.characters
+                setItem(newItems)
+                }
+              }
+              />
+          </AutoLayout>
+        </>
+        )
+      })}
     </AutoLayout>
 
   )
